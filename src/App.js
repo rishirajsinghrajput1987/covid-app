be299e9aee2c2f424e42b1a48e6c16a365b98313
rishi <rishirajput1987@gmail.com>
2021-08-23 17:06:25 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import CovidTracker from './components/CovidTracker';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Covid Tracker App</h1>
+      <CovidTracker />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CovidTracker.js
@@ -0,0 +1,61 @@
+import react, { useEffect, useState } from 'react';
+
+const CovidTracker = () => {
+
+    const [data, setData] = useState([]);
+
+    const trackData = async () => {
+        const res = await fetch("https://data.covid19india.org/data.json");
+        const data = await res.json();
+        setData(data.statewise);
+        console.log(data);
+    }
+
+    useEffect(() => {
+        trackData();
+    })
+
+    return (
+        <>
+        {
+            data.map((dataItems) => 
+                 (
+                    <div className="state-wrapper">
+                        <h2>{dataItems.state === "Total" ? "India" : dataItems.state} Covid Data</h2>
+                        <div className="covid-list">
+                            <ul>
+                                <li>
+                                    <div>
+                                        <small>{dataItems.lastupdatedtime}</small><br/>
+                                        <strong>Active </strong>
+                                    </div>
+                                    <span className="active-case"> {dataItems.active}</span>    
+                                </li>
+                                <li>
+                                    <div>
+                                    <small>{dataItems.lastupdatedtime}</small><br/>
+                                    <strong>Recovered </strong>
+                                    </div>
+                                    <span className="recovered-case"> {dataItems.recovered}</span>    
+                                </li>
+                                <li>
+                                    <div>
+                                    <small>{dataItems.lastupdatedtime}</small><br/>
+                                    <strong>Death </strong>
+                                    </div>
+                                    <span className="death-case"> {dataItems.deaths}</span>    
+                                </li>
+                            </ul>
+                        </div>
+                    </div>   
+                )
+            )
+        }
+
+
+        
+        </>
+    )
+}
+
+export default CovidTracker;
