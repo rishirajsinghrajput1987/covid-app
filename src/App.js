607f6941b62b3a867ac532d2d7a06ce78aa42e49
rishi <rishirajput1987@gmail.com>
2021-08-29 13:02:04 +0530

    resolved issue in route overthe github

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
     <div>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Header />
         <div className="main-wrapper">
           <AllRouters />
