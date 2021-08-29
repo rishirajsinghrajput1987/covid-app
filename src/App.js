780bd8c8c12cc981a95ff61d63b232a3bfbab625
rishi <rishirajput1987@gmail.com>
2021-08-29 14:36:57 +0530

    fixed the homepage path in package json

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import CovidTracker from "./components/CovidTracker";
 import "./App.css";
 import Header from "./components/Header";
 import AllRouters from "./Routers/AllRouters";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Header />
-        <div className="main-wrapper">
-          <AllRouters />
-        </div>
-      </BrowserRouter>
+      <Header />
+      <div className="main-wrapper">
+        <AllRouters />
+      </div>
     </div>
   );
 }
